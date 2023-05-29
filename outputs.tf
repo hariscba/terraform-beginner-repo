04c8fd4a16212c5937ebd0cd6ef80f9c2fa53d9c
hariscba <harish.bennalli@gmail.com>
2023-05-29 22:00:00 +0530

    Update main.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,5 @@
-output "name" {
-  value       = aws_instance.web-server.associate_public_ip_address
-  sensitive   = false
-  description = "Print out my public IP Address"
-  depends_on  = [aws_instance.web-server]
-}
+
+
+
+
 
