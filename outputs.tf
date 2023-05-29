aa6c8e3ba00544a334ecc5624213cc7d63044ccd
hariscba <87900380+hariscba@users.noreply.github.com>
2023-05-29 17:09:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "name" {
+  value       = aws_instance.web-server.associate_public_ip_address
+  sensitive   = false
+  description = "Print out my public IP Address"
+  depends_on  = [aws_instance.web-server]
+}
 
