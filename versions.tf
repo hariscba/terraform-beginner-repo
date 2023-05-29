f677866af22b4b5b98a09ed255a330d5b7f9d5d5
hariscba <87900380+hariscba@users.noreply.github.com>
2023-05-29 16:13:46 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,10 @@
 terraform {
-  required_providers {
+  required_version = ">= 1.0"
+  
+   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.35"
     }
   }
-}
-
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
 }
