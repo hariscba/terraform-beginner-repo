4954a5affe35554c3d025e2ad78f999465053d79
hariscba <harish.bennalli@gmail.com>
2023-05-29 22:01:35 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "publicsubnet" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-  depends_on              = [aws_internet_gateway.igw]
+  depends_on              = [aws_internet_gateway.gw]
 
   tags = {
     Name = "custompublicsubnet"
@@ -26,7 +26,7 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
@@ -35,6 +35,7 @@ resource "aws_route_table" "public_rt" {
 }
 
 #create route table association to public subnet
+
 resource "aws_route_table_association" "public_rt_assoc" {
   subnet_id      = aws_subnet.publicsubnet.id
   route_table_id = aws_route_table.public_rt.id
@@ -51,20 +52,6 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "internetGW"
   }
-}
-
-#create key pair
-resource "aws_key_pair" "tf-key-pair" {
-key_name = "tf-key-pair"
-public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits  = 4096
-}
-resource "local_file" "tf-key" {
-content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair"
 }
 
 #create custom security group
@@ -110,31 +97,31 @@ resource "aws_security_group" "allow_traffic" {
 }
 
 #create an ec2 instance and install apache web server
-resource "aws_instance" "web_server" {
+resource "aws_instance" "web-server" {
   ami                         = "ami-0715c1897453cabd1"
   instance_type               = "t2.micro"
   availability_zone           = "us-east-1a"
-  key_name                    = "tf-key-pair"
+  key_name                    = "mykeypairname"
   subnet_id                   = aws_subnet.publicsubnet.id
   associate_public_ip_address = true
   security_groups             = [aws_security_group.allow_traffic.id]
-  user_data                   = <<-EOF
-                                #!/bin/bash
-                                sudo apt-get update -y
-                                sudo apt-get install -y httpd
-                                sudo systemctl start httpd
-                                echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-                                EOF
+  user_data                   = 
+                <<-EOF
+                  #!/bin/bash
+                  yum update -y
+                  yum install -y httpd
+                  yum systemctl start httpd
+                  EOF
 
   tags = {
     Name = "web_server"
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.web_server.public_ip
+output "name" {
+  value       = aws_instance.web-server.associate_public_ip_address
   sensitive   = false
   description = "Print out my public IP Address"
-  depends_on  = [aws_instance.web_server]
+  depends_on  = [aws_instance.web-server]
 }
 
